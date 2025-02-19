aa9f6de2d274284a5e01c70e3141ae287bc10fb9
vitoriano <alanvitoriano8@gmail.com>
2025-02-18 21:48:01 -0300

    feat:add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500  rounded-xl">
+      <h1 className="text-red-500">product page</h1>
+      <Button>vtn</Button>
+      <input placeholder="vtn melhor do mundo" />
+    </div>
+  );
+};
+
+export default ProductPage;
